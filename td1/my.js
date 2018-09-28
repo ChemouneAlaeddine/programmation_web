1e9162a3c132594fd34e51173fb1446c012b15b4
chemoune <chemounetorres@yahoo.fr>
2018-09-28 15:20:33 +0200

    td_3

--- td1/my.js
+++ td1/my.js
@@ -7,55 +7,34 @@ function article(quartier,rues) {
   this.rues = rues;
 }
 
-console.log("1");
-
-
-
 $(document).ready(function(){
-	console.log("2");
-	function ab(){	
-		console.log("3");
-		$.ajax({
-			url: "http://odata.bordeaux.fr/v1/databordeaux/sigadrgrav/?format=json",
-			dataType: "jsonp",
-			jsonpCallback: "jsonCallback"
-		});
-	}
-});
+	$.getJSON('http://odata.bordeaux.fr/v1/databordeaux/sigadrgrav/?format=json&callback=?', function(result){
 
-function jsonCallback(json){
-	console.log("4");
-	loadData(json);
-}
+		for (var i = 0; i < result.d.length; i++) {
 
-function loadData(json){
+			if(!isInArray(quartiers, result.d[i].quartier))
+				quartiers.push(result.d[i].quartier);	
+		}
 
-	/*for (var i = 0; i < json.d.length; i++) {
-
-		if(!isInArray(used_cards, img1))
-			quartiers.push(json.d[i].quartier);	
-	}*/
-
-
-	/*for (var i = 0; i < quartiers.length; i++) {
-		for (var j = 0; j < json.d.length; j++) {
-			if (json.d[i].quartier === json.d[j].quartier){
-				rues.push(json.d[i].adresse);
+		for (var i = 0; i < quartiers.length; i++) {
+			for (var j = 0; j < result.d.length; j++) {
+				if (result.d[i].quartier === result.d[j].quartier){
+					rues.push(result.d[j].adresse);
+				};
 			};
-		};
-		articles.push(new article(json.d[i].quartier,rues));
-		rues = [];
-	}*/
+			articles.push(new article(result.d[i].quartier,rues));
+			rues = [];
+		}
 
+		for (var i = 0; i < articles.length; i++){
+			$("div.row").append("<div class=\"col-md-4\"><h2>"+articles[i].quartier+"</h2><hr><p><a href=\"#\" class=\"btn btn-primary my-2\">Categorie 1</a><a href=\"#\" class=\"btn btn-secondary my-2\">auteur</a><a href=\"#\" class=\"btn btn-secondary my-2\">01/01/01</a></p><ul id=\"number_"+i+"\">     </ul></div>");
 
-	/*for (var i = 0; i < articles.length; i++){
-		$("div.row").append("<div class=\"col-md-4\"><h2>"+articles[i].quartier+"</h2><hr><p><a href=\"#\" class=\"btn btn-primary my-2\">Categorie 1</a><a href=\"#\" class=\"btn btn-secondary my-2\">auteur</a><a href=\"#\" class=\"btn btn-secondary my-2\">01/01/01</a></p><p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p></div>");
-	}*/
-
-
-
-
-}
+			for (var j = 0; j < articles[i].rues.length; j++) {
+				$("ul#number_"+i).append("<li>"+articles[i].rues[j]+"</li>")
+			}
+		}
+	});
+});
 
 function isInArray(array, search)
 {
--- td3/my.js
+++ td3/my.js
@@ -1,47 +1,27 @@
 $(document).ready(function(){
+	
 	// toilets
 	$('#btn-toilets').click(function(){
-		$.ajax({
-			url: "http://odata.bordeaux.fr/v1/databordeaux/sigsanitaire/?format=json",
-			dataType: "jsonp",
-			jsonpCallback: "jsonCallback"
-		});
+
+		$.getJSON('http://odata.bordeaux.fr/v1/databordeaux/sigsanitaire/?format=json&callback=?', function(result){
+   			$("ul#data-list").empty();
+			for (var i = 0; i < result.d.length; i++) {
+				$("ul#data-list").append("<li><br>PartitionKey: "+result.d[i].PartitionKey +"<br>RowKey: "+ result.d[i].RowKey +"<br>Timestamp: "+ result.d[i].Timestamp +"<br>entityid: "+ result.d[i].entityid +"<br>cle: "+ result.d[i].cle +"<br>adresse: "+ result.d[i].adresse +"<br>nom: "+ result.d[i].nom +"<br>options: "+ result.d[i].options +"<br>quartier: "+ result.d[i].quartier +"<br>typologie: "+ result.d[i].typologie +"<br>x_long: "+ result.d[i].x_long +"<br>y_lat: "+ result.d[i].y_lat +"<br>geometrie: "+ result.d[i].geometrie +"<br>num_quartier: "+ result.d[i].num_quartier+"<br><hr></li>");
+			}
+   		});
+
 	});
 
 	// air jeux
 	$('#btn-kidareas').click(function(){
-		$.ajax({
-			url: "http://odata.bordeaux.fr/v1/databordeaux/airejeux/?format=json",
-			dataType: "jsonp",
-			jsonpCallback: "jsonCallback2"
+
+		$.getJSON('http://odata.bordeaux.fr/v1/databordeaux/airejeux/?format=json&callback=?', function(result){
+		$("ul#data-list").empty();
+			for (var i = 0; i < result.d.length; i++) {
+				$("ul#data-list").append("<li><br>PartitionKey: "+result.d[i].PartitionKey +"<br>RowKey: "+ result.d[i].RowKey +"<br>Timestamp: "+ result.d[i].Timestamp +"<br>entityid: "+ result.d[i].entityid +"<br>cle: "+ result.d[i].cle +"<br>adresse: "+ result.d[i].adresse +"<br>nom: "+ result.d[i].nom +"<br>options: "+ result.d[i].options +"<br>quartier: "+ result.d[i].quartier +"<br>typologie: "+ result.d[i].typologie +"<br>x_long: "+ result.d[i].x_long +"<br>y_lat: "+ result.d[i].y_lat +"<br>geometrie: "+ result.d[i].geometrie +"<br>num_quartier: "+ result.d[i].num_quartier+"<br><hr></li>");
+			}
 		});
+
 	});
 
-
 });
-
-//====================================== Toilets ====================================
-
-function jsonCallback(json){
-	loadData(json);
-}
-
-function loadData(json){
-	$("ul#data-list").empty();
-	for (var i = 0; i < json.d.length; i++) {
-		$("ul#data-list").append("<li><br>PartitionKey: "+json.d[i].PartitionKey +"<br>RowKey: "+ json.d[i].RowKey +"<br>Timestamp: "+ json.d[i].Timestamp +"<br>entityid: "+ json.d[i].entityid +"<br>cle: "+ json.d[i].cle +"<br>adresse: "+ json.d[i].adresse +"<br>nom: "+ json.d[i].nom +"<br>options: "+ json.d[i].options +"<br>quartier: "+ json.d[i].quartier +"<br>typologie: "+ json.d[i].typologie +"<br>x_long: "+ json.d[i].x_long +"<br>y_lat: "+ json.d[i].y_lat +"<br>geometrie: "+ json.d[i].geometrie +"<br>num_quartier: "+ json.d[i].num_quartier+"<br><hr></li>");
-	}
-}
-
-//====================================== Jeux ========================================
-
-function jsonCallback2(json){
-	loadData2(json);
-}
-
-function loadData2(json){
-	$("ul#data-list").empty();
-	for (var i = 0; i < json.d.length; i++) {
-		$("ul#data-list").append("<li><br>PartitionKey: "+json.d[i].PartitionKey +"<br>RowKey: "+ json.d[i].RowKey +"<br>Timestamp: "+ json.d[i].Timestamp +"<br>entityid: "+ json.d[i].entityid +"<br>cle: "+ json.d[i].cle +"<br>nature: "+ json.d[i].nature +"<br>nom: "+ json.d[i].nom +"<br>age_min: "+ json.d[i].age_min +"<br>age_max: "+ json.d[i].age_max +"<br>nombre_jeux: "+ json.d[i].nombre_jeux +"<br>x_long: "+ json.d[i].x_long +"<br>y_lat: "+ json.d[i].y_lat +"<br>geometrie: "+ json.d[i].geometrie +"<br>num_quartier: "+ json.d[i].num_quartier+"<br><hr></li>");
-	}
-}
